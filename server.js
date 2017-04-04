9bc1f1da363f539bbdc544bdec3b7187e9a3c9a6
mwdeal1 <mwdeal1@gmail.com>
2017-04-03 23:36:21 -0400

    fix port

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var methodOverride = require("method-override");
 
 var app = express();
 
+var port = process.env.PORT || 8080;
+
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + "/public"));
 
