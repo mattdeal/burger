843f21d0d09de525620f176aef29b49e10ad1b81
mwdeal1 <mwdeal1@gmail.com>
2017-04-03 23:48:22 -0400

    add controller and cleanup burger.js

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,42 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/", function(req, res) {
+  burger.create([
+    "name", "sleepy" //todo: get param here
+  ], [
+    req.body.name, req.body.sleepy
+  ], function() {
+    res.redirect("/");
+  });
+});
+
+router.put("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update({
+    sleepy: req.body.sleepy // todo: get params here
+  }, condition, function() {
+    res.redirect("/");
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -2,18 +2,17 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-  selectAll: function(cb) {
+  all: function(cb) {
     orm.selectAll(function(res) {
       cb(res);
     });
   },
-  // The variables cols and vals are arrays.
-  insertOne: function(burgerName, cb) {
+  create: function(burgerName, cb) {
     orm.insertOne(burgerName, function(res) {
       cb(res);
     });
   },
-  updateOne: function(objColVals, condition, cb) {
+  update: function(objColVals, condition, cb) {
     orm.updateOne(objColVals, condition, function(res) {
       cb(res);
     });
